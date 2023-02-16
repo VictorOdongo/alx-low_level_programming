a8f640b4252ae34d60e1e5501e58391d789c45c3
VictorOdongo <victorodongo397@gmail.com>
2023-02-16 14:56:29 +0300

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - entry point
  * 
- * return:0 (success)
+ * return: 0 (success)
  */
 int main(void)
 {
@@ -10,4 +10,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
