48cf7f2980354f99ebbf8530f8895e8a7a7bea1f
VictorOdongo <victorodongo397@gmail.com>
2023-02-16 14:11:31 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+    /* Print the message using puts function */
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
+}
+
