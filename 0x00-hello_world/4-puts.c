172d01508769b637e3711d6b6ffd96581b420161
VictorOdongo <victorodongo397@gmail.com>
2023-02-16 15:42:16 +0300

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -A program that prints a line with puts function 
- * Return: 0 (success)
+ * main -Entry
+ * Return: 0 
  */
 int main(void)
 {
-/* Print the message using puts function */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
