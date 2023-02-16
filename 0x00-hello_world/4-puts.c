fe1a8074c9bec85ffa0c57f676cc7ae79762ed83
VictorOdongo <victorodongo397@gmail.com>
2023-02-16 15:26:17 +0300

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function 
+ * main -A program that prints a line with puts function 
  * Return: 0 (success)
  */
 int main(void)
