40cb8fcf89769b03c947671a06a554e306074e65
VictorOdongo <victorodongo397@gmail.com>
2023-02-17 18:47:33 +0300

    combo.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-int n;
+int i;
 for (i = 48; i < 58; i++)
 {
 putchar(i);
@@ -14,7 +14,7 @@ if (i != 57)
 putchar(',');
 putchar(' ');
 }
+}
 putchar('\n');
 return (0);
-}
 }
