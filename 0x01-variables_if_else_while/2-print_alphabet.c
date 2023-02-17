9e5f7440438aed32db12fbbdd8957bc128954ecf
VictorOdongo <victorodongo397@gmail.com>
2023-02-17 16:29:53 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
