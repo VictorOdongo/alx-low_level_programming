0a7785ce5abb6d87b73e41c24b9474498cc61ac5
VictorOdongo <victorodongo397@gmail.com>
2023-02-17 16:38:36 +0300

    alphabets small and large

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+for (i = 65; i < 91; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
