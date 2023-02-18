ed7491fe58ec93d10d54a2a548de1c06c7daa12e
VictorOdongo <victorodongo397@gmail.com>
2023-02-18 09:39:49 +0300

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
+
