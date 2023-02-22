a737b08c7ef85ec8934d39352a54e54433da256f
VictorOdongo <victorodongo397@gmail.com>
2023-02-22 23:18:05 +0300

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,19 @@
  */
 void times_table(void)
 {
-int i;
-int result;
-for (i = 0; i <= 10; i++)
+int i, j, result;
+for (i = 0; i <= 9; i++)
 {
-result = 9 * i;
-if (result < 10)
+for (j = 0; j <= 9; j++)
 {
+result = i * j;
+if (j == 0)
+{
+_putchar('0');
+}
+else if (result < 10)
+{
+_putchar(' ');
 _putchar(result + '0');
 }
 else
@@ -19,8 +25,15 @@ else
 _putchar((result / 10) + '0');
 _putchar((result % 10) + '0');
 }
+if (j < 9)
+{
 _putchar(',');
 _putchar(' ');
 }
+else
+{
 _putchar('\n');
+}
+}
+}
 }
