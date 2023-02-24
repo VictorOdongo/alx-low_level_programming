8035554ae7988b4cf1f2377ed6db002777207aa5
VictorOdongo <victorodongo397@gmail.com>
2023-02-24 17:17:49 +0300

    except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - 0 - 9, except 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
