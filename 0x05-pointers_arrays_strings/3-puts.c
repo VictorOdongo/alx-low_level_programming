f658f467c9b0fcc2b29e6e41c4ca5a378b5e1605
VictorOdongo <victorodongo397@gmail.com>
2023-02-27 15:09:14 +0300

    String followed by new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints string followed by new line
+ * @str: String
+ * Return: Printed string
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
