6b176b191ab61e891c68118853b1f3caec9228d6
VictorOdongo <victorodongo397@gmail.com>
2023-03-06 16:53:49 +0300

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * print_chessboard - Prints chessboard
+ * @a: set of characters in array
+ * Return: contents of array in form of chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
